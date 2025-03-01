9bab3de46a35f2482a7ae8684b6e37f1b78c0dd1
grigor <musoyangrigor251@gmail.com>
2025-03-01 17:55:41 +0400

    testing 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,14 @@
+#include <iostream>
 #include "vector.h"
 
 int main() {
-    my::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    vec.push_back(11);
-    vec.push_back(12);
-    vec.push_back(13);
-    vec.push_back(14);
-    vec.push_back(15);
+    const my::vector<bool> vec = {true, false, true};
 
-    vec.pop_back();
-    vec.pop_back();
-    vec.pop_back();
-    vec.pop_back();
-    vec.pop_back();
-    vec.pop_back();
-
-    vec.print();
+    for (int i = 0; i < vec.size(); i++) {
+       if (vec[i]) {
+           std::cout << i << std::endl;
+       }
+    }
 
 
     return 0;
