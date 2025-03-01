3d607537d46217733c83a3b352f3fc26e7e9099d
grigor <musoyangrigor251@gmail.com>
2025-03-01 17:52:42 +0400

    testing 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
 #include "vector.h"
-#include "string.h"
 
 int main() {
-    const my::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    my::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vec.push_back(11);
+    vec.push_back(12);
+    vec.push_back(13);
+    vec.push_back(14);
+    vec.push_back(15);
 
-    for (int i = 0; i < vec.size(); i++) {
-        std::cout << vec[i] << std::endl;
-    }
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+
+    vec.print();
+
 
     return 0;
 }
