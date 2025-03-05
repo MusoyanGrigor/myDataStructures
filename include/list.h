c0f790bbff7d980e1067c4a1c1573bde7cf3443c
grigor <musoyangrigor251@gmail.com>
2025-03-05 12:52:06 +0400

    feat: Implement basic linked list with push/pop operations
    - Added Node class with data and next/prev pointers
    - Implemented List class with the following functions:
      - push_back(): Add element at the end
      - push_front(): Add element at the beginning
      - pop_back(): Remove element from the end
      - pop_front(): Remove element from the beginning

--- include/list.h
+++ include/list.h
@@ -1,10 +1,63 @@
 #ifndef LIST_H
 #define LIST_H
 
-namespace my {
-    template <typename T>
-    class list {
+#include <cstddef>
+#include "node.h"
 
+namespace my {
+    template<typename T>
+    class list {
+    public:
+        list() : m_head(nullptr), m_tail(nullptr) {}
+
+        void push_back(const T& value) {
+            Node<T> *newNode = new Node(value);
+            if (!m_head) {
+                m_head = m_tail = newNode;
+            } else {
+                m_tail->m_next = newNode;
+                newNode->m_prev = m_tail;
+                m_tail = newNode;
+            }
+            m_size++;
+        }
+
+        void push_front(const T& value) {
+            Node<T>* newNode = new Node(value);
+            if (!m_head) {
+                m_head = m_tail = newNode;
+            } else {
+                newNode->m_next = m_head;
+                m_head->m_prev = newNode;
+                m_head = newNode;
+            }
+            m_size++;
+        }
+
+        void pop_back() {
+            if (!m_tail) return;
+            const Node<T>* temp = m_tail;
+            m_tail = m_tail->m_prev;
+            if (m_tail) m_tail->m_next = nullptr;
+            else m_head = nullptr;
+            delete temp;
+            m_size--;
+        }
+
+        void pop_front() {
+            if (!m_head) return;
+            const Node<T>* temp = m_head;
+            m_head = m_head->m_next;
+            if (m_head) m_head->m_prev = nullptr;
+            else m_tail = nullptr;
+            delete temp;
+            m_size--;
+        }
+
+    private:
+        Node<T> *m_head;
+        Node<T> *m_tail;
+        size_t m_size{};
     };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
-#include "forward_list.h"
-
+#include "list.h"
 int main() {
 
 
-    const my::forward_list<int> list = {1,2,3,4,5};
+    my::list<int> list1;
+    list1.push_back(1);
+    list1.push_back(2);
+    list1.push_back(3);
+    list1.push_back(4);
+
+    list1.push_front(5);
+    list1.push_back(6);
+    list1.push_back(7);
+
+    list1.pop_front();
+    list1.pop_back();
+    list1.pop_back();
+    list1.pop_front();
+
 
     return 0;
 }
