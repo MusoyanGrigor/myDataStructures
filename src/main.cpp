22261d89ba2a5a066ce97d894f80cf82d80d2679
grigor <musoyangrigor251@gmail.com>
2025-03-17 15:58:35 +0400

    iterator testing

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
     my::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    for (my::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
-        std::cout << *it << std::endl;
+    for (const int & it : vec) {
+        std::cout << it << std::endl;
     }
 
     return 0;
