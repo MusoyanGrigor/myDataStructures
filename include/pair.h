1f0173cf045b40e01dba759f5de1b8d1f492383d
grigor <musoyangrigor251@gmail.com>
2025-04-02 14:31:05 +0400

    Added print function for pair class

--- include/pair.h
+++ include/pair.h
@@ -1,12 +1,18 @@
 #ifndef PAIR_H
 #define PAIR_H
 
+#include <iostream>
+
 namespace my {
     template <typename T1, typename T2>
     class pair {
         pair(const pair<T1, T2> &p) : first(p.first), second(p.second) {}
 
         auto operator<=>(const pair& other) const = default;
+
+        void print() const {
+            std::cout << "(" << first << ", " << second << ")\n";
+        }
 
     private:
         T1 first;
