4ca44c8d5357572c5de061a1f37fb62f0b41152c
grigor <musoyangrigor251@gmail.com>
2025-04-02 14:28:53 +0400

    implemented my::pair<T1, T2>

---
+++ include/pair.h
@@ -0,0 +1,16 @@
+#ifndef PAIR_H
+#define PAIR_H
+
+namespace my {
+    template <typename T1, typename T2>
+    class pair {
+        pair(const pair<T1, T2> &p) : first(p.first), second(p.second) {}
+
+
+      private:
+        T1 first;
+        T2 second;
+    };
+}
+
+#endif //PAIR_H
