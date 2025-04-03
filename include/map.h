8a73800cf4f81231f3bffe85bd82f7b73c1d68b8
grigor <musoyangrigor251@gmail.com>
2025-04-03 12:08:00 +0400

    Added findIndex() helper function to improve lookup efficiency in map.h

--- include/map.h
+++ include/map.h
@@ -12,6 +12,20 @@ namespace my {
 
     private:
         my::vector<my::pair<K, V>> data;
+
+        int findIndex(const K& key) const {
+            int left = 0;
+            int right = data.size() - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (data[mid].first == key) return mid;
+                if (data[mid].first < key)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+        }
     };
 
 }
