ea33225dfc9894d9bea985ea479c3dd3215486e7
grigor <musoyangrigor251@gmail.com>
2025-04-03 12:11:52 +0400

    Added insert() function in map.h

--- include/map.h
+++ include/map.h
@@ -7,19 +7,31 @@
 namespace my {
     template <typename K, typename V>
     class map {
-
+    public:
+      void insert(const K& key, const V& value) {
+        int index = findIndex(key);
+        if (index != -1) {
+          m_data[index].second = value;
+        } else {
+            int pos = 0;
+            while (pos < m_data.size() && m_data[pos].first < key) {
+              ++pos;
+               m_data.insert(pos, my::pair<K, V>(key, value));
+            }
+        }
+      }
 
 
     private:
-        my::vector<my::pair<K, V>> data;
+        my::vector<my::pair<K, V>> m_data;
 
         int findIndex(const K& key) const {
             int left = 0;
-            int right = data.size() - 1;
+            int right = m_data.size() - 1;
             while (left <= right) {
                 int mid = left + (right - left) / 2;
-                if (data[mid].first == key) return mid;
-                if (data[mid].first < key)
+                if (m_data[mid].first == key) return mid;
+                if (m_data[mid].first < key)
                     left = mid + 1;
                 else
                     right = mid - 1;
