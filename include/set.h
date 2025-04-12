021d3320ec2dfce9f7b1a720df7aa8954ab81bde
grigor <musoyangrigor251@gmail.com>
2025-04-12 10:32:13 +0400

    implemented my::set<T>

--- include/set.h
+++ include/set.h
@@ -1,12 +1,43 @@
 #ifndef SET_H
 #define SET_H
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 namespace my {
+template <typename K>
+class set {
+private:
+    std::vector<K> data;
 
-template <typename T>
-class set{
+public:
+    void insert(const K& value) {
+        if (!contains(value)) {
+            data.push_back(value);
+            std::sort(data.begin(), data.end());
+        }
+    }
 
+    void erase(const K& value) {
+        auto it = std::find(data.begin(), data.end(), value);
+        if (it != data.end()) {
+            data.erase(it);
+        }
+    }
+
+    bool contains(const K& value) const {
+        return std::binary_search(data.begin(), data.end(), value);
+    }
+
+    void print() const {
+        for (const K& val : data) {
+            std::cout << val << " ";
+        }
+        std::cout << "\n";
+    }
 };
+
 }
 
 #endif //SET_H
