fef4d99ce0b4cef11a2adf9e3bc8a09d92011f12
grigor <musoyangrigor251@gmail.com>
2025-04-25 10:52:32 +0400

    Fix iterator implementation to support random access

--- include/iterator.h
+++ include/iterator.h
@@ -1,57 +1,51 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
+#include <iterator> // for std::random_access_iterator_tag
+
 namespace my {
     template<typename T>
     class iterator {
     public:
-        using reference_type = T&;
-        using pointer_type = T*;
         using value_type = T;
+        using reference = T&;
+        using pointer = T*;
         using difference_type = std::ptrdiff_t;
         using iterator_category = std::random_access_iterator_tag;
 
         explicit iterator(T* ptr) : m_ptr(ptr) {}
 
-        reference_type operator*() { return *m_ptr; }
+        // Dereference
+        reference operator*() const { return *m_ptr; }
+        pointer operator->() const { return m_ptr; }
 
-        pointer_type operator->() { return m_ptr; }
+        // Increment / Decrement
+        iterator& operator++() { ++m_ptr; return *this; }
+        iterator operator++(int) { iterator tmp = *this; ++(*this); return tmp; }
+        iterator& operator--() { --m_ptr; return *this; }
+        iterator operator--(int) { iterator tmp = *this; --(*this); return tmp; }
 
-        iterator& operator++() {
-            ++m_ptr;
-            return *this;
-        }
+        // Arithmetic
+        iterator operator+(difference_type n) const { return iterator(m_ptr + n); }
+        iterator operator-(difference_type n) const { return iterator(m_ptr - n); }
+        difference_type operator-(const iterator& other) const { return m_ptr - other.m_ptr; }
 
-        iterator operator++(int) {
-            iterator temp = *this;
-            ++(*this);
-            return temp;
-        }
+        iterator& operator+=(difference_type n) { m_ptr += n; return *this; }
+        iterator& operator-=(difference_type n) { m_ptr -= n; return *this; }
 
-        iterator& operator--() {
-            --m_ptr;
-            return *this;
-        }
+        reference operator[](difference_type n) const { return *(m_ptr + n); }
 
-        iterator operator--(int) {
-            iterator temp = *this;
-            --(*this);
-            return temp;
-        }
-
+        // Comparison
         bool operator==(const iterator& other) const { return m_ptr == other.m_ptr; }
         bool operator!=(const iterator& other) const { return m_ptr != other.m_ptr; }
         bool operator<(const iterator& other) const { return m_ptr < other.m_ptr; }
         bool operator>(const iterator& other) const { return m_ptr > other.m_ptr; }
-
-        iterator operator+(size_t n) const { return iterator(m_ptr + n); }
-        iterator operator-(size_t n) const { return iterator(m_ptr - n); }
-        difference_type operator-(const iterator& other) const { return m_ptr - other.m_ptr; }
-
+        bool operator<=(const iterator& other) const { return m_ptr <= other.m_ptr; }
+        bool operator>=(const iterator& other) const { return m_ptr >= other.m_ptr; }
 
     private:
         T* m_ptr;
-     };
+    };
 }
 
-#endif //ITERATOR_H
+#endif // ITERATOR_H
