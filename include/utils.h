a3545a94b55f7ee9fc0f7249deeb4194882768e9
grigor <musoyangrigor251@gmail.com>
2025-05-20 12:15:11 +0400

    implemented my forward in utils.h

--- include/utils.h
+++ include/utils.h
@@ -11,6 +11,17 @@ namespace utils {
         std::remove_reference_t<T>&& move(T&& value) {
             return static_cast<std::remove_reference_t<T>&&>(value);
         }
+
+        template <typename T>
+        T&& forward(std::remove_reference_t<T>& value) {
+            return static_cast<T&&>(value);
+        }
+
+        template <typename T>
+        T&& forward(std::remove_reference_t<T>&& value) {
+            static_assert(!std::is_lvalue_reference_v<T>);
+            return static_cast<T&&>(value);
+        }
     }
 
 }
